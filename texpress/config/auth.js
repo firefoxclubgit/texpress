41530b6d84c30e2e57de88996f5e9b73705158b7
tushararora <tushararora.cs@gmail.com>
2015-06-12 10:58:30 +0530

    Express app with social plugin

---
+++ texpress/config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '462998260524697', // your App ID
+        'clientSecret'  : '1ce91a122c0fa44a675e99e65f6b0174', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    }
+
+};
